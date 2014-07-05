0eba5a03a373db53d604f3b1ec58b00f01c74f62
Sumeet Singh <ortuna@gmail.com>
2014-07-05 00:32:55 -0400

    Variables

--- hello_world.go
+++ hello_world.go
@@ -1,6 +1,16 @@
 package main
+
 import "fmt"
 
 func main() {
-  fmt.Println("Hello world!");
+	fmt.Println("Hello world!")
+
+	b := 5
+	fmt.Println(b)
+	b = 10
+	fmt.Println(b)
+
+	var c int
+	c = 20
+	fmt.Println(c)
 }
