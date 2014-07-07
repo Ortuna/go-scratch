8de0974b51d1e86b6cf1fc468e61c6cec0379e60
Sumeet Singh <ortuna@gmail.com>
2014-07-06 22:22:16 -0400

    Maps

--- hello_world.go
+++ hello_world.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	ints := make([]int, 100)
-	for i := 0; i < len(ints); i++ {
-		fmt.Printf("%d\n", i)
+	var mapz = map[string]int{
+		"test": 123,
+		"set":  123,
 	}
+
+	fmt.Printf("%d", mapz["test"])
 }
