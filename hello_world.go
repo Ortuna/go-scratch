b1d7eac101f05f98362af6445f9311d54538d70d
Sumeet Singh <ortuna@gmail.com>
2014-07-06 21:50:52 -0400

    Slice!

--- hello_world.go
+++ hello_world.go
@@ -2,29 +2,9 @@ package main
 
 import "fmt"
 
-type Person struct {
-	firstName string
-	lastName  string
-}
-
-func printPerson(p Person) {
-	printPersonPointer(&p)
-}
-
-func printPersonPointer(p *Person) {
-	fmt.Println(p.firstName)
-	fmt.Println(p.lastName)
-}
-
 func main() {
-	p := new(Person)
-	p.firstName = "test"
-	p.lastName = "name"
-	printPersonPointer(p)
-
-	var v Person
-	v.firstName = "test2"
-	v.lastName = "name2"
-
-	printPerson(v)
+	ints := make([]int, 100)
+	for i := 0; i < len(ints); i++ {
+		fmt.Printf("%d\n", i)
+	}
 }
